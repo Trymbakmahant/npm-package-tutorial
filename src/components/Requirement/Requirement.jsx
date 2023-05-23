dd7129f09de2fbefe15607b587687b7213c9690b
Trymbak Mahant <trymbak0102mahant@gmail.com>
2023-05-23 12:48:33 +0530

    working requirement.jsx

---
+++ src/components/Requirement/Requirement.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Requirement = () => {
+  return (
+    <div>
+      <div> dofw </div>
+      <div> dofw </div>
+    </div>
+  );
+};
---
+++ src/stories/Requirement.stories.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { Requirement } from "../components/Requirement/Requirement";
+
+export default {
+  title: "Example/Requirement",
+  component: Requirement,
+  tags: ["autodocs"],
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+};
+
+export const Primary = {
+  args: {
+    //   primary: true,
+    //   label: "Button",
+    // },
+  },
+};
