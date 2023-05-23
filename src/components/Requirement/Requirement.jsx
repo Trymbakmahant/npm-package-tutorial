024d67fa5b4f950d1312879844984dd1f463a493
Trymbak Mahant <trymbak0102mahant@gmail.com>
2023-05-23 15:07:54 +0530

    Button added

--- src/components/Requirement/Requirement.jsx
+++ src/components/Requirement/Requirement.jsx
@@ -1,10 +1,62 @@
 import React from "react";
+import PropTypes from "prop-types";
+import "./Requirement.css";
 
-export const Requirement = () => {
+/**
+ * Primary UI component for user interaction
+ */
+export const Requirement = ({
+  primary,
+  backgroundColor,
+  Fun,
+  size,
+  label,
+  ...props
+}) => {
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
   return (
-    <div>
-      <div> dofw </div>
-      <div> dofw </div>
-    </div>
+    <button
+      type="button"
+      onClick={Fun}
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
+      style={backgroundColor && { backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
   );
+};
+
+Requirement.propTypes = {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary: PropTypes.bool,
+  /**
+   * What background color to use
+   */
+  backgroundColor: PropTypes.string,
+  /**
+   * How large should the button be?
+   */
+  size: PropTypes.oneOf(["small", "medium", "large"]),
+  /**
+   * Button contents
+   */
+  label: PropTypes.string.isRequired,
+  /**
+   * Optional click handler
+   */
+  onClick: PropTypes.func,
+};
+
+Requirement.defaultProps = {
+  backgroundColor: null,
+  primary: false,
+  size: "medium",
+  onClick: undefined,
 };
--- src/stories/Requirement.stories.js
+++ src/stories/Requirement.stories.js
@@ -2,19 +2,24 @@ import React from "react";
 
 import { Requirement } from "../components/Requirement/Requirement";
 
+function handleclick() {
+  window.open("https://superfluid-wavepool.vercel.app/payment");
+}
+
 export default {
   title: "Example/Requirement",
   component: Requirement,
   tags: ["autodocs"],
   argTypes: {
     backgroundColor: { control: "color" },
+    onClick: handleclick,
   },
 };
 
 export const Primary = {
   args: {
-    //   primary: true,
-    //   label: "Button",
-    // },
+    primary: true,
+    label: "Button",
+    onClick: handleclick,
   },
 };
